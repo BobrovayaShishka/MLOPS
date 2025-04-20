6922c5d03bf0ff7712414d81f6d2bdb7589649ee
BobrovayaShishka <166939759+BobrovayaShishka@users.noreply.github.com>
2025-04-20 18:35:16 +0500

    Update download.py

--- lab3/download.py
+++ lab3/download.py
@@ -1,1 +1,14 @@
+import numpy as np
+from sklearn.datasets import fetch_openml
+import pandas as pd
 
+def download_data():
+    mnist = fetch_openml('mnist_784', version=1)
+    data = pd.DataFrame(mnist.data)
+    target = pd.DataFrame(mnist.target)
+    data.to_csv("data.csv", index=False)
+    target.to_csv("target.csv", index=False)
+    print("Данные успешно загружены!")
+
+if __name__ == "__main__":
+    download_data()
