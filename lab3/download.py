17aa3218ea5fa907989a943ad4605bb9e12b6e87
BobrovayaShishka <166939759+BobrovayaShishka@users.noreply.github.com>
2025-04-20 21:48:31 +0500

    Update download.py

--- lab3/download.py
+++ lab3/download.py
@@ -1,14 +1,52 @@
-import numpy as np
-from sklearn.datasets import fetch_openml
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
 
 def download_data():
-    mnist = fetch_openml('mnist_784', version=1)
-    data = pd.DataFrame(mnist.data)
-    target = pd.DataFrame(mnist.target)
-    data.to_csv("data.csv", index=False)
-    target.to_csv("target.csv", index=False)
-    print("Данные успешно загружены!")
+    df = pd.read_csv('https://raw.githubusercontent.com/dayekb/Basic_ML_Alg/main/cars_moldova_no_dup.csv', delimiter = ',')
+    df.to_csv("cars.csv", index = False)
+    return df
 
-if __name__ == "__main__":
-    download_data()
+def clear_data(path2df):
+    df = pd.read_csv(path2df)
+    
+    cat_columns = ['Make', 'Model', 'Style', 'Fuel_type', 'Transmission']
+    num_columns = ['Year', 'Distance', 'Engine_capacity(cm3)', 'Price(euro)']
+    
+    question_dist = df[(df.Year <2021) & (df.Distance < 1100)]
+    df = df.drop(question_dist.index)
+    # Анализ и очистка данных
+    # анализ гистограмм
+    question_dist = df[(df.Distance > 1e6)]
+    df = df.drop(question_dist.index)
+    
+    # здравый смысл
+    question_engine = df[df["Engine_capacity(cm3)"] < 200]
+    df = df.drop(question_engine.index)
+    
+    # здравый смысл
+    question_engine = df[df["Engine_capacity(cm3)"] > 5000]
+    df = df.drop(question_engine.index)
+    
+    # здравый смысл
+    question_price = df[(df["Price(euro)"] < 101)]
+    df = df.drop(question_price.index)
+    
+    # анализ гистограмм
+    question_price = df[df["Price(euro)"] > 1e5]
+    df = df.drop(question_price.index)
+    
+    #анализ гистограмм
+    question_year = df[df.Year < 1971]
+    df = df.drop(question_year.index)
+    
+    df = df.reset_index(drop=True)  
+    ordinal = OrdinalEncoder()
+    ordinal.fit(df[cat_columns])
+    Ordinal_encoded = ordinal.transform(df[cat_columns])
+    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=cat_columns)
+    df[cat_columns] = df_ordinal[cat_columns]
+    df.to_csv('df_clear.csv')
+    return True
+
+download_data()
+clear_data("cars.csv")
